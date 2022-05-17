c4440b0a64d2ec6733c71d962e7c010a37ba6d85
Shashwath Uchil <shashwath.uchil@niveussolutions.com>
2022-05-17 14:16:29 +0530

    update shell code

--- sh/log.sh
+++ sh/log.sh
@@ -1,11 +1,5 @@
-# git pull
-
-# git add ../app/containers/ao_ucjfdgt90_ft/
-
-# git commit -m "log from fdgt90"
-
-# git push
-
 git config submodule.ignore all
 
 git config diff.ignoreSubmodules all
+
+ git reset .
