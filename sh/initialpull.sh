b4c8d035e92d5bed1e5947a732419e75131eb6a2
Shashwath Uchil <shashwath.uchil@niveussolutions.com>
2022-05-20 11:15:02 +0530

    initial pull new commands added

--- sh/initialpull.sh
+++ sh/initialpull.sh
@@ -1,5 +1,26 @@
-cd ../src/CardContainer2
+cd ..
 
-git submodule init 
+for VARIABLE in src/Card src/Components/Child2
+do
+    cd $VARIABLE
 
-git submodule update
+    git submodule init 
+
+    git submodule update
+
+
+    while [ ! -f .gitignore ]
+    do
+    cd ..
+    pwd
+    done
+
+
+    # if [ -f .gitignore ]   # For file "if [ -f /home/rama/file ]" -d for directory
+    # then
+    #     echo "Root Folder"
+    # else
+    #     echo "Root Not Found"
+    # fi
+
+ done
