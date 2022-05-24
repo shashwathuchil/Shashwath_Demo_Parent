eaae9108bac28a55d454c3cc27019c7bba5cc43b
Shashwath Uchil <shashwath.uchil@niveussolutions.com>
2022-05-24 12:35:56 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Card2 from './Components/Dev2/logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Card2></Card2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
